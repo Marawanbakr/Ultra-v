d2f499bb097d8b08409885dc18e9b7c2f5fce195
Ahmed Younes <cahmedc@gmail.com>
2020-05-07 07:15:49 +0200

    fix

--- src/Component/Portfolio/index.js
+++ src/Component/Portfolio/index.js
@@ -1,45 +1,63 @@
-import React ,{useState , useEffect} from 'react';
-import {PortfolioSection,PortfolioTitle,Span,PortfolioList,PortfolioItem,ImageWrapper,Image,Overlay,OverlaySpan} from './style.js'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import {
+  PortfolioSection,
+  PortfolioTitle,
+  Span,
+  PortfolioList,
+  PortfolioItem,
+  ImageWrapper,
+  Image,
+  Overlay,
+  OverlaySpan,
+} from "./style.js";
+import axios from "axios";
 const Portfolio = () => {
- 
-    const [images , setImages] = useState([])
+  const [images, setImages] = useState([]);
 
-    useEffect ( () => {
-      axios.get('js/data.json').then( res => {setImages(res.data.Portfolio) } )
+  useEffect(() => {
+    axios.get("js/data.json").then((res) => {
+      setImages(res.data.Portfolio);
+    });
+  }, []);
 
-    }, [] )
+  /* 
+  لازم تتأكد إن في 
+  images 
+  علشان تعمل
+  map 
+  عليها
+  */
+  let PortfolioImages;
+  if (images && images.length > 0) {
+    PortfolioImages = images.map((imageItem) => {
+      return (
+        <ImageWrapper key={imageItem.id}>
+          <Image src={imageItem.image} alt="" />
+          <Overlay>
+            <OverlaySpan>Show Image</OverlaySpan>
+          </Overlay>
+        </ImageWrapper>
+      );
+    });
+  } else {
+    PortfolioImages = [];
+  }
 
-     const PortfolioImages = images.map( (imageItem)=> {
-         return (
-          <ImageWrapper key={imageItem.id}>
-          <Image src={imageItem.image} alt=""/>
-            <Overlay>
-                <OverlaySpan>
-                    Show Image
-                </OverlaySpan>
-              </Overlay>  
-            </ImageWrapper>  
-         )
-     })
-
-    return (
+  return (
     <PortfolioSection>
-        <PortfolioTitle><Span>My</Span> Portfolio</PortfolioTitle>
-        <PortfolioList>
-            <PortfolioItem active>All</PortfolioItem>
-            <PortfolioItem>HTML</PortfolioItem>
-            <PortfolioItem>Photoshop</PortfolioItem>
-            <PortfolioItem>Wordpress</PortfolioItem>
-            <PortfolioItem>Mobile</PortfolioItem>
-    </PortfolioList>
-      
-      <div class="box">
-          {PortfolioImages}
-      </div>
-      
-  </PortfolioSection>
-    
-    )
-}
+      <PortfolioTitle>
+        <Span>My</Span> Portfolio
+      </PortfolioTitle>
+      <PortfolioList>
+        <PortfolioItem active>All</PortfolioItem>
+        <PortfolioItem>HTML</PortfolioItem>
+        <PortfolioItem>Photoshop</PortfolioItem>
+        <PortfolioItem>Wordpress</PortfolioItem>
+        <PortfolioItem>Mobile</PortfolioItem>
+      </PortfolioList>
+
+      <div class="box">{PortfolioImages}</div>
+    </PortfolioSection>
+  );
+};
 export default Portfolio;
